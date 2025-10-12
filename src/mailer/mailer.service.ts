5b7a461ad68414f968b3116f6e3eab6846fad936
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-12 22:05:58 +0200

    config: Mailer

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -11,7 +11,7 @@ export class MailerService {
       service:"gmail",
       auth: {
         user: process.env.EMAILUSER,
-        pass: process.env.EMAILPASS
+        pass: process.env.EMAILPASgit add
       }
     });
 
